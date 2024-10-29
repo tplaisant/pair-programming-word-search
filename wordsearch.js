4fb9ac6af3af989725f0a00157f857c7075cf414
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-28 18:36:19 -0600

    first commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//@SilentYell (Nik Hixson)
+//@tplaisant (Tiago Plaisant)
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      console.log(l);
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+  
+    for (let col = 0; col < letters[0].length; col++) {
+      let newArray = [];
+      for (let row = 0; row < letters.length; row++) {
+        console.log(letters[row][col]);
+        newArray.push(letters[row][col]);
+        if (newArray.join('').includes(word)) {
+          return true;
+        }
+      }
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
